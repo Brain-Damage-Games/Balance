e7096ce833f675209c0437ef9451639a32638430
fatemehzarebahari <75903639+fatemehzarebahari@users.noreply.github.com>
2022-08-17 12:18:49 +0330

    changing attachable

--- Balance/Assets/Scripts/Attachable.cs
+++ Balance/Assets/Scripts/Attachable.cs
@@ -23,6 +23,10 @@ public class Attachable : MonoBehaviour
         if (CheckAttachablity() && aim.tag == "Aim")
             aim.GetComponent<AttachReceiver>().Attach(gameObject);
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("collides");
+    }
     public void Detach()
     {
         aim.GetComponent<AttachReceiver>().Detach(gameObject);
@@ -33,17 +37,19 @@ public class Attachable : MonoBehaviour
         Vector3 dest = transform.position;
         Vector3 direction = Vector3.Normalize(dest - camera);
         Ray ray = new Ray(dest, direction);
+        Ray ray2 = new Ray(dest, -direction);
         RaycastHit hitInfo;
 
-        if(Physics.Raycast(ray, out hitInfo, distance))
+        if(Physics.Raycast(ray, out hitInfo, distance) || (Physics.Raycast(ray2, out hitInfo, distance)))
         {
-            //Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
             aim = hitInfo.collider.gameObject;
             return true;
         }
         else
         {
-            //Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.blue);
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.blue);
+            Debug.DrawLine(ray2.origin, ray2.origin + ray2.direction * 100, Color.blue);
             return false;
 
         }
