9c8a62b21c2d15bf30990f46188d2e542314d810
ElyasTH <elyastashakkori@gmail.com>
2022-08-25 19:18:46 +0430

    Revert "box placed in levels"
    This reverts commit d1b17526faeac59c3aac39e3b8a871e0272ac650.

--- Balance/Assets/Scripts/ShelfSorting.cs
+++ Balance/Assets/Scripts/ShelfSorting.cs
@@ -7,6 +7,7 @@ public class ShelfSorting : MonoBehaviour
     List<GameObject> allObjects;
     List<GameObject> seenObjects;
 
+    [SerializeField]
     GameObject[] buttons;
 
     float space, objectsScale;
@@ -43,18 +44,18 @@ public class ShelfSorting : MonoBehaviour
         shelfStartPosition = transform.position.z - transform.localScale.z / 2f;
         shelfEndPosition = transform.position.z + transform.localScale.z / 2f;  
 
-        // buttons[0].transform.position = new Vector3
-        // (
-        //     transform.localPosition.x,
-        //     transform.position.y + objectsScale,
-        //     shelfStartPosition + objectsScale / 2
-        // );
-        // buttons[1].transform.position = new Vector3
-        // (
-        //     transform.localPosition.x,
-        //     transform.position.y + objectsScale,
-        //     shelfEndPosition - objectsScale / 2
-        // );
+        buttons[0].transform.position = new Vector3
+        (
+            transform.localPosition.x,
+            transform.position.y + objectsScale,
+            shelfStartPosition + objectsScale / 2
+        );
+        buttons[1].transform.position = new Vector3
+        (
+            transform.localPosition.x,
+            transform.position.y + objectsScale,
+            shelfEndPosition - objectsScale / 2
+        );
 
 
         Sort();
@@ -171,13 +172,13 @@ public class ShelfSorting : MonoBehaviour
                 transform.localScale.z / 11f
             ) ;
 
-        // for(int i = 0; i < buttons.Length; i++)
-        //     buttons[i].transform.localScale = new Vector3
-        //     (
-        //         transform.localScale.z / 11f,
-        //         transform.localScale.z / 11f,
-        //         transform.localScale.z / 11f
-        //     );
+        for(int i = 0; i < buttons.Length; i++)
+            buttons[i].transform.localScale = new Vector3
+            (
+                transform.localScale.z / 11f,
+                transform.localScale.z / 11f,
+                transform.localScale.z / 11f
+            );
         
     }
 
